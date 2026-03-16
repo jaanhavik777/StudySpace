d6904e65107d436f8a2044b48352fa3a2288452b
Krishna Manoj <krishnamanoj615@gmail.com>
2026-03-16 21:43:40 +0530

    change case

--- backend/routes/pomodoroRoutes.js
+++ backend/routes/pomodoroRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Pomodoro = require("../models/temp");
+const Pomodoro = require("../models/Pomodoro");
 
 router.get("/:user", async (req, res) => {
   try {
